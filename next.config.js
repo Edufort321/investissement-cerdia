92af99501ff7a8b7fde3fd600ed5f63febdb2572
Edufort321 <eric.dufort@cerdia.ai>
2025-06-26 12:30:55 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,26 @@ const nextConfig = {
     locales: ['fr', 'en'],
     defaultLocale: 'fr',
   },
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com', // Images Amazon produit
+      },
+      {
+        protocol: 'https',
+        hostname: 'a.co', // Lien court (si redirige vers image)
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com', // S3 ou autre stockage
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazon.ca', // optionnel selon les images utilisées
+      },
+    ],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
